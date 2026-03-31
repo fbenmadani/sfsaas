6dfb42acc2816651f417b64d07cdf466235b9100
fbenmadani <miragetech@gmail.com>
2026-03-31 18:19:47 +0100

    Register TenancyServiceProvider in bootstrap/providers.php

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -2,8 +2,11 @@
 
 use App\Providers\AppServiceProvider;
 use App\Providers\FortifyServiceProvider;
+use App\Providers\TenancyServiceProvider;
+
 
 return [
     AppServiceProvider::class,
     FortifyServiceProvider::class,
+    TenancyServiceProvider::class,
 ];
