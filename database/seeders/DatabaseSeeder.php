8fa876c01f07a5cb1dda2759c439bca3f724a7c0
fbenmadani <miragetech@gmail.com>
2026-04-14 10:17:20 +0100

    fix user seed imports

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Database\Seeders\UserSeeder;
+
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +18,8 @@ class DatabaseSeeder extends Seeder
         $this->call([
             UserSeeder::class,
         ]);
+
+
 
         // User::factory()->create([
         //     'name' => 'Test User',
