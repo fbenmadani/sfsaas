1b5a6dff4d97080220fd2a792adcae4f229199cb
fbenmadani <miragetech@gmail.com>
2026-04-14 10:53:12 +0100

    add route to manage users

--- routes/web.php
+++ routes/web.php
@@ -14,12 +14,14 @@ foreach (config('tenancy.central_domains') as $domain) {
         Route::view('/features', 'marketing.features')->name('marketing.features');
         Route::view('/pricing', 'marketing.pricing')->name('marketing.pricing');
         Route::view('/about', 'marketing.about')->name('marketing.about');
-
+        
         Route::middleware(['auth', 'verified'])->group(function () {
             Route::view('dashboard', 'dashboard')->name('dashboard');
-
+            Route::livewire('/users', 'users.index')->name('users.index');
             Route::middleware('admin')->group(function () {
-                Route::get('admin/users', function () { return 'Users'; })->name('admin.users.index');
+               //Route::get('admin/users', function () { return 'Users'; })->name('admin.users.index');
+               Route::livewire('admin/users', 'admin.users.index')->name('admin.users.index');
+               //Route::view('admin', 'admin')->name('admin.users.index');
             });
         });
 
