3bd64830b81814e1e7829e9b7defabc0c0b8715a
fbenmadani <miragetech@gmail.com>
2026-04-19 09:39:24 +0100

    Test tennat identification

---
+++ tests/Feature/TenantIdentificationTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use function Pest\Laravel\get;
+
+it('shows the custom 404 tenant page when tenant cannot be identified by subdomain', function () {
+    // We use a subdomain of a central domain that doesn't exist as a tenant
+    $response = get('http://unknown.sfsaas.test');
+
+    $response->assertStatus(404);
+    $response->assertSee('Workspace Not Found');
+    $response->assertSee('Oops! We couldn\'t find the workspace you\'re looking for.', false);
+});
+
+it('shows the custom 404 tenant page when tenant cannot be identified by domain', function () {
+    // We use a domain that is not a central domain and doesn't exist as a tenant
+    $response = get('http://completely-unknown-domain.test');
+
+    $response->assertStatus(404);
+    $response->assertSee('Workspace Not Found');
+});
