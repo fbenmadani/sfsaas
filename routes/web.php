0f7b6cb2f0dfe53bd3265980881ad0ec3ab66875
fbenmadani <miragetech@gmail.com>
2026-04-19 09:38:25 +0100

    re order routes

--- routes/web.php
+++ routes/web.php
@@ -2,33 +2,26 @@
 
 use Illuminate\Support\Facades\Route;
 
-
-
-
-
 foreach (config('tenancy.central_domains') as $domain) {
     Route::domain($domain)->group(function () {
-    
-        
+
         Route::view('/', 'marketing.home')->name('home');
         Route::view('/features', 'marketing.features')->name('marketing.features');
         Route::view('/pricing', 'marketing.pricing')->name('marketing.pricing');
         Route::view('/about', 'marketing.about')->name('marketing.about');
-        
+        Route::livewire('/sign-up', 'pages::account.sign-up')->name('account.sign-up');
+
         Route::middleware(['auth', 'verified'])->group(function () {
             Route::view('dashboard', 'dashboard')->name('dashboard');
             Route::livewire('/users', 'users.index')->name('users.index');
             Route::middleware('admin')->group(function () {
-               //Route::get('admin/users', function () { return 'Users'; })->name('admin.users.index');
-               Route::livewire('admin/users', 'admin.users.index')->name('admin.users.index');
-               //Route::view('admin', 'admin')->name('admin.users.index');
+                // Route::get('admin/users', function () { return 'Users'; })->name('admin.users.index');
+                Route::livewire('admin/users', 'admin.users.index')->name('admin.users.index');
+                // Route::view('admin', 'admin')->name('admin.users.index');
             });
         });
 
-
     });
 }
-
-
 
 require __DIR__.'/settings.php';
