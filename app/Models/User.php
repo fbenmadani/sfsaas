44f7e3a0cc2177bfff8e24602230ad4978ff4fa3
fbenmadani <miragetech@gmail.com>
2026-04-29 19:48:09 +0100

    add belongs to a tenants

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             ->map(fn ($word) => Str::substr($word, 0, 1))
             ->implode('');
     }
+
+    public function tenant()
+    {
+        return $this->belongsTo(Tenant::class);
+    }
 }
